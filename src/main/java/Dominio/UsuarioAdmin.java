b5f1c4e03cdb045e192b79275adab8c28b66a479
Fransheska <fransheskavalenzuela@gmail.com>
2024-05-27 21:34:27 -0400

    base con diagrama visual paradigm

---
+++ src/main/java/Dominio/UsuarioAdmin.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+public class UsuarioAdmin {
+	public InicioSesion _unnamed_InicioSesion_;
+	public Pedidos _unnamed_Pedidos_;
+
+	public void pedidosAdministrador() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void eliminarPedidos() {
+		throw new UnsupportedOperationException();
+	}
+}
