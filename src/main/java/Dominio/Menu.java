1bccd789437d30fdc9552f84df67d240a27c9999
Fransheska <fransheskavalenzuela@gmail.com>
2024-05-27 21:34:27 -0400

    base con diagrama visual paradigm

---
+++ src/main/java/Dominio/Menu.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+public class Menu {
+	private Object _entrada;
+	private Object _platoDeFondo;
+	private Object _bebestible;
+	private Object _ensalada;
+	private Object _postre;
+	public Pedidos _unnamed_Pedidos_;
+
+	public void agregarMenu() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/main/java/Dominio/Pedidos.java
@@ -0,0 +1,16 @@
+package Dominio;
+
+public class Pedidos {
+	public Cliente _unnamed_Cliente_;
+	public UsuarioAdmin _unnamed_UsuarioAdmin_;
+	public Menu _unnamed_Menu_;
+	public Pagos _unnamed_Pagos_;
+
+	public void agregarPedido() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void codigoRetiro() {
+		throw new UnsupportedOperationException();
+	}
+}
