6fc24b6198cf2e70519ab5eee395ace1ae7bb588
Ignacio <ivaldebenito52@gmail.com>
2024-06-10 17:32:36 -0400

    commit6 fixed

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        ContralodorLogin contralodorLogin = new ContralodorLogin();
-        Menu menu = new Menu(contralodorLogin);
-        menu.mostrarMenu();
-    }
-}
