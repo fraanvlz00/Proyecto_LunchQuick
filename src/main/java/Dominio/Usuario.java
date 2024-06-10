0e7c2ee9f843ed909eb295a93c1f8c1722f33909
Ignacio <ivaldebenito52@gmail.com>
2024-06-10 17:33:34 -0400

    commit

--- src/main/java/Dominio/Usuario.java
+++ src/main/java/Dominio/Usuario.java
@@ -12,7 +12,7 @@ public class Usuario {
     private String correoElectronico;
     private String contraseña;
     private String almuerzoComprado;
-    private JsonNode root;
+    JsonNode root;
     private ObjectMapper mapper;
     private String rut; // Agregar atributo rut
 
@@ -66,38 +66,5 @@ public class Usuario {
         this.rut = rut;
     }
 
-    // Método para iniciar sesión
-    public Usuario iniciarSesion() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese su correo electrónico: ");
-        String correo = scanner.nextLine();
-        System.out.println("Ingrese su contraseña: ");
-        String contraseña = scanner.nextLine();
 
-        try {
-            Iterator<JsonNode> usuarios = root.get("usuarios").elements();
-            while (usuarios.hasNext()) {
-                JsonNode usuarioNode = usuarios.next();
-
-                if (usuarioNode.has("correoElectronico") && usuarioNode.has("contraseña")) {
-                    String jsonCorreo = usuarioNode.get("correoElectronico").asText();
-                    String jsonContraseña = usuarioNode.get("contraseña").asText();
-
-                    if (jsonCorreo.equals(correo) && jsonContraseña.equals(contraseña)) {
-                        Usuario usuario = new Usuario(correo, contraseña, "");
-                        if (usuarioNode.has("rut")) {
-                            usuario.setRut(usuarioNode.get("rut").asText()); // Obtener el RUT del JSON
-                        }
-                        System.out.println("Inicio de sesión exitoso.");
-                        return usuario;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Error al procesar el inicio de sesión: " + e.getMessage());
-        }
-
-        System.out.println("Correo electrónico o contraseña incorrectos.");
-        return null;
-    }
 }
