438ee30593bd1b0c92b03af0e0645afdacb45d8d
Ignacio <ivaldebenito52@gmail.com>
2024-06-30 16:58:43 -0400

    commit7 añadiendo ventanas

---
+++ src/main/java/Ventanas/DiaSemana.java
@@ -0,0 +1,12 @@
+package Ventanas;
+
+import javax.swing.*;
+
+public class DiaSemana {
+    private JPanel jpDiaSemana;
+    private JButton btnDiaSigiente;
+    private JComboBox comboBox1;
+    private JButton btnDiaVolver;
+    private JLabel lblDiaElgido;
+    private JLabel lblLunchDia;
+}
